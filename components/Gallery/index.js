b17e8e13934fcd2c564e157a89f35255072f6368
_Zinhle <zbuhlungu1@gmail.com>
2021-09-28 11:04:24 +0200

    added some graded pictures

--- components/Gallery/index.js
+++ components/Gallery/index.js
@@ -3,61 +3,84 @@
 
 
 const Gallery = (props) => {
+
+
     return (
         <><div className="container">
             <div className="row">
                 <div className="col-lg-12 ">
-                        <h1>Gallery</h1>
-                </div>                                                              </div>
+                    <h1>Gallery</h1>
+                </div>
+            </div>
             <div className="row">
                 <div className="col-12 col-sm-12 col-md-6 col-lg-4 custom grid">
                     <div className="wpo-img-gallery">
-                        <img src="images/R10Images/matome.png" alt="" />
+                        <img src="images/R10Images/matome.jpg" alt="" />
                     </div>
                 </div>
                 <div className="col-12 col-sm-12 col-md-6 col-lg-4 custom grid">
                     <div className="wpo-img-gallery">
-                        <img src="images/R10Images/matome2.png" alt="" />
+                        <img src="images/R10Images/matome2.jpg" alt="" />
                     </div>
                 </div>
                 <div className="col-12 col-sm-12 col-md-6 col-lg-4 custom grid">
                     <div className="wpo-img-gallery">
-                        <img src="images/R10Images/sivu.png" alt="" />
+                        <img src="images/R10Images/sivu.jpg" alt="" />
                     </div>
                 </div>
                 <div className="col-12 col-sm-12 col-md-6 col-lg-4 custom grid">
                     <div className="wpo-img-gallery">
-                        <img src="images/R10Images/sivu2.png" alt="" />
+                        <img src="images/R10Images/school.jpg" alt="" />
                     </div>
                 </div>
                 <div className="col-12 col-sm-12 col-md-6 col-lg-4 custom grid">
                     <div className="wpo-img-gallery">
-                        <img src="images/R10Images/mandisa.png" alt="" />
+                        <img src="images/R10Images/mandisa.jpg" alt="" />
                     </div>
                 </div>
                 <div className="col-12 col-sm-12 col-md-6 col-lg-4 custom grid">
                     <div className="wpo-img-gallery">
-                        <img src="images/R10Images/mandisa2.png" alt="" />
+                        <img src="images/R10Images/mandisa2.jpg" alt="" />
                     </div>
                 </div>
                 <div className="col-12 col-sm-12 col-md-6 col-lg-4 custom grid">
                     <div className="wpo-img-gallery">
-                        <img src="images/R10Images/mandisa3.png" alt="" />
+                        <img src="images/R10Images/mandisa3.jpg" alt="" />
                     </div>
                 </div>
                 <div className="col-12 col-sm-12 col-md-6 col-lg-4 custom grid">
                     <div className="wpo-img-gallery">
-                        <img src="images/R10Images/akani.png" alt="" />
+                        <img src="images/R10Images/akani.jpg" alt="" />
                     </div>
                 </div>
                 <div className="col-12 col-sm-12 col-md-6 col-lg-4 custom grid">
                     <div className="wpo-img-gallery">
-                        <img src="images/R10Images/matome3.png" alt="" />
+                        <img src="images/R10Images/matome3.jpg" alt="" />
                     </div>
                 </div>
             </div>
-        </div></>
+                    
+                       
+                        <div className="col-lg-12 custom grid">
+                            <div className="col-sm-4"></div>
+                            <a
+                                className=" col-sm-3 theme-btn" href="/">
+                                See More
+                            </a>
+                        </div>
+                       
+        </div>
 
+            <style jsx>{`
+
+                .seeMore {
+
+                left: "500px",
+
+
+                }
+                        `}
+            </style></>
     )
 }
 
