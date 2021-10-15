aab2bb8a423853888824c4e4f492c5f9cc099c39
_Zinhle <zbuhlungu1@gmail.com>
2021-10-15 17:02:55 +0200

    p

--- components/header/index.js
+++ components/header/index.js
@@ -4,76 +4,84 @@ import HeaderTopbar from '../HeaderTopbar'
 import MobileMenu from '../../components/MobileMenu'
 import { motion } from 'framer-motion'
 const Header = () => {
-    const SubmitHandler = (e) =>{
+    const SubmitHandler = (e) => {
         e.preventDefault()
-     }
+    }
 
-    return(	
-	<div className="middle-header header-style-3">
-        {/* <HeaderTopbar/> */}
-        <div className="container">
-            <div className="header-content">
-            <div className="row">
-                <div className="col-lg-3 col-md-4 col-sm-4 col-4">
-                    <motion.div 
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{delay: .5}}
-                    exit={{ opacity: 0 }}
-                    className="logo">
-                        <Link href="/HomePage3" title="">
-                           <a><img src='images/fundifoundation-logo-blue.png' alt=""/></a>
-                        </Link>
-                    </motion.div>
-                </div>
-                <div className="col-lg-8 d-lg-block d-none">
-                    <nav>
-                        <ul>
-                            <motion.li
-                            initial={{ opacity: 0, y: -20 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            exit={{ opacity: 0, y: 10 }}
-                            transition={{ delay: .2}}
-                            ><Link className="active" href="/HomePage3" title="">Home</Link>
-                                {/* <ul>
+    return (
+        <div className="middle-header header-style-3">
+            {/* <HeaderTopbar/> */}
+            <div className="container">
+                <div className="header-content">
+                    <div className="row">
+                        <div className="col-lg-3 col-md-4 col-sm-4 col-4">
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ delay: .5 }}
+                                exit={{ opacity: 0 }}
+                                className="logo">
+                                <Link href="/HomePage3" title="">
+                                    <a><img src='images/fundifoundation-logo-blue.png' alt="" /></a>
+                                </Link>
+                            </motion.div>
+                        </div>
+                        <div className="col-lg-8 d-lg-block d-none">
+                            <nav>
+                                <ul>
+                                    <motion.li
+                                        initial={{ opacity: 0, y: -20 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        exit={{ opacity: 0, y: 10 }}
+                                        transition={{ delay: .2 }}
+                                    ><Link className="active" href="/HomePage3" title="">Home</Link>
+                                        {/* <ul>
                                     <li><Link className="active" href="/">Home style 1</Link></li>
                                     <li><Link href="/HomePage2">Home style 2</Link></li>
                                     <li><Link href="/HomePage3">Home style 3</Link></li>
                                 </ul> */}
-                            </motion.li>
-                            <motion.li
-                             initial={{ opacity: 0, y: -20 }}
-                             animate={{ opacity: 1, y: 0 }}
-                             exit={{ opacity: 0, y: 10 }}
-                             transition={{ delay: .3}}
-                          ><Link href="/AboutPage" title="">About</Link></motion.li>
-                            <motion.li
-                             initial={{ opacity: 0, y: -20 }}
-                             animate={{ opacity: 1, y: 0 }}
-                             exit={{ opacity: 0, y: 10 }}
-                             transition={{ delay: .4}}
-                            ><Link href="/CasePage" title="">Causes</Link>
-                                <ul>
-                                    <li><Link href="/CaseSinglePageR10" title="">R10 Goes a Long way</Link></li>
-                                    <li><Link href="/CaseSinglePageFund" title="">FundiFund 12412</Link></li>
-                                    <li><Link href="/CaseSinglePageRebuild" title="">Unite4Schools</Link></li>
-                                    <li><Link href="/CaseSinglePageLeaders" title="">Leaders4Learners</Link></li>
-                                    <li><Link href="/CaseSinglePageFundiCsi" title="">Fundi CSI</Link></li>
+                                    </motion.li>
+                                    <motion.li
+                                        initial={{ opacity: 0, y: -20 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        exit={{ opacity: 0, y: 10 }}
+                                        transition={{ delay: .3 }}
+                                    ><Link href="/AboutPage" title="">About</Link></motion.li>
+                                    <motion.li
+                                        initial={{ opacity: 0, y: -20 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        exit={{ opacity: 0, y: 10 }}
+                                        transition={{ delay: .4 }}
+                                    ><Link href="/CasePage" title="">Causes</Link>
+                                        <ul>
+                                            <li><Link href="/CaseSinglePageR10" title="">R10 Goes a Long way</Link></li>
+                                            <li><Link href="/CaseSinglePageFund" title="">FundiFund 12412</Link></li>
+                                            <li><Link href="/CaseSinglePageRebuild" title="">Unite4Schools</Link></li>
+                                            <li><Link href="/CaseSinglePageLeaders" title="">Leaders4Learners</Link></li>
+                                            <li><Link href="/CaseSinglePageFundiCsi" title="">Fundi CSI</Link></li>
+                                            <li><Link href="/PrivacyPage" title="">Privacy Policy</Link></li>
 
-                                </ul>
-                            </motion.li>
-                            <motion.li
-                             initial={{ opacity: 0, y: -20 }}
-                             animate={{ opacity: 1, y: 0 }}
-                             exit={{ opacity: 0, y: 10 }}
-                             transition={{ delay: .5}}
-                            ><Link href="/EventPage" title="">Press</Link>
-                                {/* <ul>
+                                        </ul>
+                                    </motion.li>
+                                    <motion.li
+                                        initial={{ opacity: 0, y: -20 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        exit={{ opacity: 0, y: 10 }}
+                                        transition={{ delay: .5 }}
+                                    ><Link href="/EventPage" title="">Press</Link>
+                                        {/* <ul>
                                     <li><Link href="/EventPage" title="">Event</Link></li>
                                     <li><Link href="/EventPageSingle" title="">Event Single</Link></li>
                                 </ul> */}
-                            </motion.li>
-                            {/* <li><Link href="/" title="">Pages</Link>
+                                    </motion.li>
+                                    <motion.li
+                                        initial={{ opacity: 0, y: -20 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        exit={{ opacity: 0, y: 10 }}
+                                        transition={{ delay: .5 }}
+                                    ><li><Link href="/PrivacyPage" title="">Privacy Policy</Link></li>
+                                    </motion.li>
+                                    {/* <li><Link href="/" title="">Pages</Link>
                                 <ul>
                                     <li><Link href="/AboutPage" title="">About</Link></li>
                                     <li><Link href="/DonatePage" title="">Donate</Link></li>
@@ -81,7 +89,7 @@ const Header = () => {
                                     <li><Link href="/ErrorPage" title="">Error 404</Link></li>
                                 </ul>
                             </li> */}
-                            {/* <li><Link href="/BlogPage">Blog</Link>
+                                    {/* <li><Link href="/BlogPage">Blog</Link>
                                 <ul>
                                     <li><Link href="/BlogPage">Blog</Link></li>
                                     <li><Link href="/BlogPageLeft">Blog Left sidebar</Link></li>
@@ -95,24 +103,24 @@ const Header = () => {
                                     </li>
                                 </ul>
                             </li> */}
-                            <motion.li
-                             initial={{ opacity: 0, y: -20 }}
-                             animate={{ opacity: 1, y: 0 }}
-                             exit={{ opacity: 0, y: 10 }}
-                             transition={{ delay: .6}}
-                            ><Link href="/ContactPage" title="">Contact</Link></motion.li>
-                        </ul>
-                    </nav>
-                </div>
-                <motion.div 
-                 initial={{ opacity: 0, y: -20 }}
-                 animate={{ opacity: 1, y: 0 }}
-                 exit={{ opacity: 0, y: 10 }}
-                 transition={{ delay: .7}}
-                className="col-lg-1 col-md-6 col-sm-6 col-6">
-                    <div className="contact">
-                        <div className="cart-search-contact">
-                            {/* <div className="header-search-form-wrapper">
+                                    <motion.li
+                                        initial={{ opacity: 0, y: -20 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        exit={{ opacity: 0, y: 10 }}
+                                        transition={{ delay: .6 }}
+                                    ><Link href="/ContactPage" title="">Contact</Link></motion.li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <motion.div
+                            initial={{ opacity: 0, y: -20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 10 }}
+                            transition={{ delay: .7 }}
+                            className="col-lg-1 col-md-6 col-sm-6 col-6">
+                            <div className="contact">
+                                <div className="cart-search-contact">
+                                    {/* <div className="header-search-form-wrapper">
                                 <button className="search-toggle-btn"><i className="fi flaticon-magnifying-glass"></i></button>
                                 <div className="header-search-form">
                                     <form onSubmit={SubmitHandler}>
@@ -123,7 +131,7 @@ const Header = () => {
                                     </form>
                                 </div>
                             </div> */}
-                            {/* <div className="mini-cart">
+                                    {/* <div className="mini-cart">
                                 <button className="cart-toggle-btn"> <i className="fi flaticon-shopping-bag"></i> <span className="cart-count">02</span></button>
                                 <div className="mini-cart-content">
                                     <div className="mini-cart-items">
@@ -154,19 +162,19 @@ const Header = () => {
                                     </div>
                                 </div>
                             </div> */}
+                                </div>
+                            </div>
+                        </motion.div>
+                        <div className="col-md-2 col-sm-2 col-2">
+                            <MobileMenu />
                         </div>
                     </div>
-                </motion.div>
-                <div className="col-md-2 col-sm-2 col-2">
-                    <MobileMenu/>
+
+                    <div className="clearfix"></div>
                 </div>
+
             </div>
-               
-                <div className="clearfix"></div>
-            </div>
-            
         </div>
-    </div>
     )
 }
 
