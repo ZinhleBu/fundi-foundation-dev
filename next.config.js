cbf1926052a194f364a2447f4f26c4c71ac1072e
_Zinhle <zbuhlungu1@gmail.com>
2021-10-19 19:15:33 +0200

    next.cofig changed

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 
-const webpack = require('webpack')
 
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.experiments = { topLevelAwait: true };
-    config.plugins.push(
-        new webpack.ProvidePlugin({
-            '$': 'jquery',
-            'jQuery': 'jquery',
-            
-        }),
-        require("./public/script")
-
-    )
+   
     return config;
   },
  
