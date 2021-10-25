276f8a8229c509efc178049775022825ba8b29e0
_Zinhle <zbuhlungu1@gmail.com>
2021-10-25 11:58:37 +0200

    world img

--- components/world/index.js
+++ components/world/index.js
@@ -9,7 +9,7 @@ const WorldSection = (props) => {
                     <div className="col-12">
                         <div className="wpo-world-section">
                             <h2>Register for 2022 registration fees funding</h2>
-                            <Link href="/RegistrationPage"><a><img src='images/team/1.png' alt=""/> Register </a></Link>
+                            <Link href="/RegistrationPage"><a><img src='/images/team/1.png' alt=""/> Register </a></Link>
                         </div>
                     </div>
                 </div>
