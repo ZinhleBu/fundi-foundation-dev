772846f500e70138d839eb18960dd311f9d5cdc7
_Zinhle <zbuhlungu1@gmail.com>
2021-10-26 17:03:49 +0200

    error fixed

--- components/event2/index.js
+++ components/event2/index.js
@@ -146,7 +146,7 @@ const EventSection2 = (props) => {
                         className="col-lg-4 col-md-6 col-sm-12 col-12 custom-grid">
                         <div className="wpo-event-item">
                             <div className="wpo-event-img">
-                                <img src='/public/images/event/article5.jpg' alt="school-bag-handover" />
+                                <img src='images/event/article5.jpg' alt="school-bag-handover" />
                                 
                             </div>
                             <div className="wpo-event-text">
