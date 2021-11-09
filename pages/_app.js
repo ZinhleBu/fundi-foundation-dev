770830f0d89cbd531762c7b51dbf733ac9c04a89
_Zinhle <zbuhlungu1@gmail.com>
2021-11-09 11:29:08 +0200

    script fixed

--- pages/_app.js
+++ pages/_app.js
@@ -28,13 +28,13 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* Global site tag (gtag.js) - Google Analytics  */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-RTDDEWLE8B"></script>
-        <Script>
+        <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
 
           gtag('config', 'G-RTDDEWLE8B');
-        </Script>
+        </script>
       </Head>
       <motion.div
         initial={{ opacity: 0 }}
