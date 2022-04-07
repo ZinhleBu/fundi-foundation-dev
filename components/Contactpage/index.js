f3a09fb446bd254a61b45b93d56dc550f3321ce5
_Zinhle <zbuhlungu1@gmail.com>
2022-04-07 16:41:04 +0200

    Email adress added

--- components/Contactpage/index.js
+++ components/Contactpage/index.js
@@ -62,6 +62,8 @@ const Contactpage = () => {
                                 <div className="col-lg-4 col-md-6 col-sm-12 col-12">
                                     <div className="info-item">
                                         <h2>fund@fundi.co.za</h2>
+                                        <h2>fundifund@fundi.co.za</h2>
+
                                         <div className="info-wrap">
                                             <div className="info-icon-2">
                                                 <i className="fi flaticon-envelope"></i>
@@ -75,6 +77,7 @@ const Contactpage = () => {
                                 <div className="col-lg-4 col-md-6 col-sm-12 col-12">
                                     <div className="info-item">
                                         <h2>Contact: 011 670 6188</h2>
+                                        <h2>Call Centre: 0860 55 55 44</h2>
                                         <div className="info-wrap">
                                             <div className="info-icon-3">
                                                 <i className="ti-headphone-alt"></i>
